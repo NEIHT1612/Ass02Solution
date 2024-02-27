2e6b42132546c6b1d72e1c3a07bb0378d66c2963
NEIHT1612 <146853091+NEIHT1612@users.noreply.github.com>
2024-02-28 02:20:09 +0700

    DB
    DB asm2

---
+++ DBAss2.sql
@@ -0,0 +1,15 @@
+﻿INSERT INTO Member (MemberId, Email, CompanyName, City, Country, Password)
+VALUES 
+(1, 'thiendt@gmail.com', 'Google', 'Vinh', 'VietNam', '123'),
+(2, 'minhpq@gmail.com', 'FPT', 'HaNoi', 'VietNam', '123'),
+(3, 'cuonghv@gmail.com', 'FPT', 'HoaBinh', 'VietNam', '123'),
+(4, 'vynt@gmail.com', 'NASA', 'Hue', 'VietNam', '123'),
+(5, 'thanhpm@gmail.com', 'NextTech', 'HoChiMinh', 'VietNam', '123'),
+(6, 'quatbt@gmail.com', 'Google', 'Vinh', 'VietNam', '123');
+
+INSERT INTO Product (ProductId, CategoryId, ProductName, Weight, UnitPrice, UnitsInStock) 
+VALUES 
+(1, 3, 'Water', '100', 200.5000, 3),
+(2, 2, 'Candy', '70', 15000.0000, 146),
+(3, 3, 'Cake', '123', 20000.0000, 456),
+(4, 2, 'Milk', '69', 1000.0000, 235);
